3172e905ef5b3c29b7ca8359a7e5299a916fe5b9
mmontivero <mmontivero@tgv.com.ar>
2025-07-14 11:33:24 -0300

    fechasd

--- src/pages/admin/AdminPanel.jsx
+++ src/pages/admin/AdminPanel.jsx
@@ -150,7 +150,7 @@ const handleUpdatePrice = async () => {
         <tbody>
           {turnos.map(t => (
             <tr key={t._id}>
-              <td>{t.date}</td>
+               <td>{formatearFecha(t.date)}</td>
               <td>
                 {t.timeSlots.map(s => (
                   <span key={s.time} className="badge bg-secondary me-1">
