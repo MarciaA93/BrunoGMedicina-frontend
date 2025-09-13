bf951ab017aa4aaa427fc2e08d7e6f4bf0dc283c
mmontivero <mmontivero@tgv.com.ar>
2025-09-13 11:33:43 -0300

    true

--- src/pages/Turnero.jsx
+++ src/pages/Turnero.jsx
@@ -11,7 +11,7 @@ const MERCADOPAGO_API = `${API_BASE_URL}/api/mercadopago/create_preference`;
 
 function Turnero() {
   // 🔧 Cambiá esto a true para mostrar el cartel y bloquear el componente
-  const [enMantenimiento] = useState(false);
+  const [enMantenimiento] = useState(true);
 
   if (enMantenimiento) {
     return (
