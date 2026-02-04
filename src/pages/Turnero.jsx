f4429006e51ab92c883e3a6366d84955b2c285a2
mmontivero <mmontivero@tgv.com.ar>
2026-02-03 22:04:23 -0300

    cambio en packs

--- src/pages/Turnero.jsx
+++ src/pages/Turnero.jsx
@@ -150,8 +150,8 @@ function Turnero() {
             <h5 className="text-violet">PACK TERAPÉUTICO PREMIUM:</h5>
             <p>
               <strong>¡OFERTA!</strong> en un pago:<br />
-              • 2 sesiones de masaje completo a $60.000<br />
-              • 4 sesiones de masaje completo a $100.000<br />
+              • 2 sesiones de masaje completo (Ver al reservar)<br />
+              • 4 sesiones de masaje completo (Ver al reservar)<br />
               Tenés 2 meses para agendar las sesiones. <br />
               Comprando el pack, al agendar la primera sesión, las restantes se coordinan con el masajista.
             </p>
